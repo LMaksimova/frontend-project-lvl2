df57c5f877253a38897262659adb2b06f26406a0
Lyudmila Maksimova <idzenni@gmail.com>
2022-07-22 23:46:58 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-//const { Command } = require('commander');
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
  
+
 program.parse();
